5d7d78b19acc4af6c012b25dbfe210b3aebfb972
mayagembom <mayagembom>
2022-05-21 16:33:42 +0200

    Initial Commit

---
+++ Friends-Trivia-Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friends-Trivia-Game
+//
+//  Created by user216459 on 5/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
